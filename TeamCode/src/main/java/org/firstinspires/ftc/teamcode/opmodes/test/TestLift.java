1c2588583dc3a431e323b3471e3a9f1b777bdd1f
Hong Son Nguyen <sonnguyenhong302@gmail.com>
2024-06-28 09:06:55 +0700

    Modified TestLift OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/TestLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/TestLift.java
@@ -6,6 +6,7 @@ import org.firstinspires.ftc.teamcode.subsystems.Lift;
 
 public class TestLift extends LinearOpMode {
     private Lift lift;
+    private int mode = 1;
     @Override
     public void runOpMode() {
         lift = new Lift(hardwareMap);
@@ -13,7 +14,10 @@ public class TestLift extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            lift.setMotor(gamepad1.left_stick_y, gamepad1.right_stick_y);
+            if (gamepad1.left_bumper) mode = 1;
+            else if (gamepad1.right_bumper) mode = 2;
+            if (mode == 1) lift.setMotor(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
+            else lift.setMotor(-gamepad1.left_stick_y, -gamepad1.left_stick_y);
         }
     }
 }
