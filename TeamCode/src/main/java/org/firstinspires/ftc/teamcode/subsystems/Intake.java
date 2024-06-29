0df6202860d7986d7da4839a7f71ed17d0fd66ec
Phạm Thành Tuyên <149477072+phm-tuyenn@users.noreply.github.com>
2024-06-30 00:06:55 +0700

    add comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -8,17 +8,19 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Intake {
     private DcMotor rollerMotor1, rollerMotor2;
     private Servo leftDeployServo, rightDeployServo;
-
+    // init motor for intake
     public Intake(HardwareMap hardwareMap) {
         this.rollerMotor1 = hardwareMap.get(DcMotor.class, "rollerMotor1");
         this.rollerMotor2 = hardwareMap.get(DcMotor.class, "rollerMotor2");
+        
         this.rollerMotor1.setDirection(DcMotorSimple.Direction.REVERSE);
     }
-
+    // set power for both motor
     public void setRollSpeed(double speed) {
         rollerMotor1.setPower(speed);
         rollerMotor2.setPower(speed);
     }
+    //get 
     public double getRollSpeed() {
         return rollerMotor1.getPower();
     }
